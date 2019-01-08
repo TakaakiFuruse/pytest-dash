10c37e1c47419045acd0fe75d53156c4f3ea2065
t4rk1n <t4rk@outlook.com>
2019-01-08 18:39:10 -0500

    :bulb: Add import_app docstring.

--- pytest_dash/utils.py
+++ pytest_dash/utils.py
@@ -71,6 +71,23 @@ def wait_for_property_to_equal(
 
 
 def import_app(app_file):
+    """
+    Import a dash application from a module.
+    The import path is in dot notation to the module.
+    The variable named app will be returned.
+
+    *Example*
+
+        >>> app = import_app('my_app.app')
+
+    Will import the application in module `app` of the package `my_app`.
+
+    :param app_file: Path to the app (dot-separated).
+    :type app_file: str
+    :raise: pytest_dash.errors.NoAppFoundError
+    :return: App from module.
+    :rtype: dash.Dash
+    """
     try:
         app_module = runpy.run_module(app_file)
         app = app_module['app']
