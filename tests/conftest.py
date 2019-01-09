f07bb330c86935aa1003bc2dbd0c70ef952311e1
t4rk1n <t4rk@outlook.com>
2019-01-08 23:10:56 -0500

    :shirt: Fix conftest.py linting.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,3 @@
-# pylint: disable=missing-docstring, unused-import
+# pylint: disable=missing-docstring, unused-import, wildcard-import, unused-wildcard-import  # noqa: E501
 from pytest_dash.fixtures import dash_threaded, dash_subprocess  # noqa: F401
-from pytest_dash.behaviors import *
+from pytest_dash.behaviors import *  # noqa: F401, F403
