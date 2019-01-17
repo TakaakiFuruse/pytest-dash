7eba31eb5430812dc82a8db0c446d170a693f563
t4rk1n <t4rk@outlook.com>
2019-01-17 16:15:41 -0500

    :shirt: Group dash imports (ungrouped-imports)

--- test_apps/component_gallery.py
+++ test_apps/component_gallery.py
@@ -1,9 +1,10 @@
 # pylint: disable=missing-docstring
 import dash
-import dash_html_components as html
-import dash_core_components as dcc
 from dash.dependencies import Output, Input
 from dash.exceptions import PreventUpdate
+
+import dash_html_components as html
+import dash_core_components as dcc
 
 app = dash.Dash(__name__)
 
