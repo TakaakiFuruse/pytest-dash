ee7a79d285f97126e4fef36a4c6761c640097627
t4rk1n <t4rk@outlook.com>
2019-01-19 14:15:57 -0500

    :white_check_mark: Add WriteCommandBehavior test (custom command)

--- tests/test_custom_behavior.py
+++ tests/test_custom_behavior.py
@@ -14,25 +14,42 @@ def test_custom_behavior(testdir):
             @add_behavior('NUMBER "+" NUMBER')
             def add(n1, n2):
                 return int(n1) + int(n2)
+            
+            @add_behavior('"Write"i variable "in" element', kind='command')
+            def write_command(value, element):
+                # Same as enter in but user defined.
+                element.send_keys(value)
     '''
     )
     testdir.makeini('''
     [pytest]
     webdriver = Chrome
+    log_cli=true
     ''')
     testdir.makefile(
         '.yml',
-        test_hello_world='''
-        TestHelloWorld:
+        test_additional_behavior='''
+        TestAdditionBehavior:
             application:
                 path: test_apps.simple_app
             event:
                 - 'enter 2 + 2 in #value'
             outcome:
                 - '#value.value should be 4'
+        TestWriteCommandBehavior:
+            application:
+                path: test_apps.simple_app
+            parameters:
+                value:
+                    default: printed value
+            event:
+                - 'write $value in #value'
+            outcome:
+                - '#value.value should be "printed value"'
         Tests:
-            - TestHelloWorld
+            - TestAdditionBehavior
+            - TestWriteCommandBehavior
         '''
     )
     result = testdir.runpytest_subprocess()
-    result.assert_outcomes(passed=1)
+    result.assert_outcomes(passed=2)
