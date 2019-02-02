624f1d15fb4df94a759cc892ae5d81b964603ef2
t4rk1n <t4rk@outlook.com>
2019-02-02 13:32:00 -0500

    :white_check_mark: Add test_invalid_start_raises_subprocess

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -1,17 +1,21 @@
 # pylint: disable=missing-docstring
-import dash
 import pytest
 
 from pytest_dash.errors import DashAppLoadingError, NoAppFoundError
 from pytest_dash.application_runners import import_app
 
 
-def test_invalid_start_raises(dash_threaded):
-    app = dash.Dash(__name__)
+def test_invalid_start_raises_threaded(dash_threaded):
 
     # Start the server without setting the layout.
     with pytest.raises(DashAppLoadingError):
-        dash_threaded(app, start_timeout=1)
+        dash_threaded(import_app('test_apps.no_layout_app'), start_timeout=1)
+
+
+def test_invalid_start_raises_subprocess(dash_subprocess):
+
+    with pytest.raises(DashAppLoadingError):
+        dash_subprocess('test_apps.no_layout_app', port=8051)
 
 
 def test_no_app_found():
