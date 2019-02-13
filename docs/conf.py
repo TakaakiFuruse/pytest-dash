808c1f56c463b91e177df97f2184dc1d73ce54ca
t4rk1n <t4rk@outlook.com>
2019-02-12 20:17:28 -0500

    :bug: Fix documentation title, Fixes #62

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-from pytest import __version__ as version
+from pytest_dash import __version__ as version
 
 # -- Project information -----------------------------------------------------
 
